bb8bd51606c332f118784f080adf58693ad4a253
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-05-22 12:49:50 -0300

    Finaliza Backend

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -2,10 +2,12 @@ const PORT = 3003;
 const express = require('express')
 const server = express()
 const allowCors = require('./cors');
+const queryParser = require('express-query-int');
 
 server.use(express.urlencoded({ extended: true }))
 server.use(express.json())
 server.use(allowCors)
+server.use(queryParser())
 
 server.listen(PORT, _ => {
   console.log(`BACKEND is running on port ${PORT}`)

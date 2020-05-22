68674dcca55e041e8b959f440e7d3c0abd840ef7
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-05-22 13:14:48 -0300

    Finaliza Backend

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,14 +1,18 @@
-const PORT = 3003;
+const port = 3003
+
+const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
-const allowCors = require('./cors');
-const queryParser = require('express-query-int');
+const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 
-server.use(express.urlencoded({ extended: true }))
-server.use(express.json())
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
 server.use(allowCors)
 server.use(queryParser())
 
-server.listen(PORT, _ => {
-  console.log(`BACKEND is running on port ${PORT}`)
+server.listen(port, function() {
+    console.log(`BACKEND is running on port ${port}.`)
 })
+
+module.exports = server 
